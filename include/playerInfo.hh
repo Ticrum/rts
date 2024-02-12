c6ca5fef30f9329ea2a95fe7296d6a86bb08367b
thomas.barbe <thomas.barbe@aldrin.efrits.fr>
2024-02-12 16:45:43 +0100

    just bad name2

--- include/playerInfo.hh
+++ include/playerInfo.hh
@@ -12,21 +12,21 @@ namespace ef
     {
     public:
         PlayerInfo();
-        void makePath(shared_ptr<Unit> unit, Pos dest, MoveType moveType);
-        void stopUnit(shared_ptr<Unit> unit);
-        void setTarget(shared_ptr<Unit> unit, shared_ptr<Unit> other);
-        void setTarget(shared_ptr<Unit> unit, shared_ptr<Building> other);
+        void makePath(std::shared_ptr<Unit> unit, Pos dest, MoveType moveType);
+        void stopUnit(std::shared_ptr<Unit> unit);
+        void setTarget(std::shared_ptr<Unit> unit, std::shared_ptr<Unit> other);
+        void setTarget(std::shared_ptr<Unit> unit, std::shared_ptr<Building> other);
         void computeActions(double timePassed);
-        void placeBuilding(shared_ptr<Building> building);
-        void produce(shared_ptr<ProdBuilding> producer, shared_ptr<Unit> unitToProd);
-        void produce(shared_ptr<TechBuilding> producer, shared_ptr<Tech> unitToProd);
-        bool destroyUnit(shared_ptr<Unit> unit, bool isOther);
-        bool destroyBuilding(shared_ptr<Building> building, bool isOther);
-        void addOther(shared_ptr<Unit> unit);
-        void addOther(shared_ptr<Building> building);
-        bool isInVision(shared_ptr<Object> obj);
-        vector<shared_ptr<Unit>> selectUnit(Pos start, Pos end);
-        vector<shared_ptr<Building>> selectBuilding(Pos start, Pos end);
+        void placeBuilding(std::shared_ptr<Building> building);
+        void produce(std::shared_ptr<ProdBuilding> producer, std::shared_ptr<Unit> unitToProd);
+        void produce(std::shared_ptr<TechBuilding> producer, std::shared_ptr<Tech> unitToProd);
+        bool destroyUnit(std::shared_ptr<Unit> unit, bool isOther);
+        bool destroyBuilding(std::shared_ptr<Building> building, bool isOther);
+        void addOther(std::shared_ptr<Unit> unit);
+        void addOther(std::shared_ptr<Building> building);
+        bool isInVision(std::shared_ptr<Object> obj);
+        std::vector<std::shared_ptr<Unit>> selectUnit(Pos start, Pos end);
+        std::vector<std::shared_ptr<Building>> selectBuilding(Pos start, Pos end);
         void addMoneyGain(int money);
         void loseMoneyGain(int money);
 
@@ -35,11 +35,11 @@ namespace ef
         int moneyGain;
         int totalEnergy;
         int actualEnergy;
-        vector<shared_ptr<Building>> buildings;
-        vector<shared_ptr<Unit>> units;
-        vector<shared_ptr<Unit>> otherUnits;
-        vector<shared_ptr<Buildings>> otherBuildings;
-        shared_ptr<Building> rallyPoint;
+        std::vector<std::shared_ptr<Building>> buildings;
+        std::vector<std::shared_ptr<Unit>> units;
+        std::vector<std::shared_ptr<Unit>> otherUnits;
+        std::vector<std::shared_ptr<Buildings>> otherBuildings;
+        std::shared_ptr<Building> rallyPoint;
         Map map;
         Map visionMap;
     };
@@ -50,19 +50,19 @@ namespace ef
         ServerPlayersInfo();
         void makePath(int unitId, Pos dest, MoveType moveType, int PlayerId);
         void stopUnit(int unitId, int PlayerId);
-        void setTarget(int unitId, shared_ptr<Unit> other, int PlayerId);
-        void setTarget(int unitId, shared_ptr<Building> other, int PlayerId);
+        void setTarget(int unitId, std::shared_ptr<Unit> other, int PlayerId);
+        void setTarget(int unitId, std::shared_ptr<Building> other, int PlayerId);
         void computeActions(double timePassed);
-        void placeBuilding(string building, int PlayerId);
-        void produce(int producerId, string unitToProd, int PlayerId, bool isTech);
+        void placeBuilding(std::string building, int PlayerId);
+        void produce(int producerId, std::string unitToProd, int PlayerId, bool isTech);
         bool destroyUnit(int unitId, bool isOther, int PlayerId);
         bool destroyBuilding(int buildingId, bool isOther, int PlayerId);
 
     private:
-        vector<PlayerInfo> playersInfo;
+        std::vector<PlayerInfo> playersInfo;
         Map trueMap;
-        vector<shared_ptr<Building>> neutralBuildings;
-        shared_ptr<UdpServer> serverUdp;
+        std::vector<std::shared_ptr<Building>> neutralBuildings;
+        std::shared_ptr<UdpServer> serverUdp;
     };
 
     class ClientPlayerInfo
@@ -74,8 +74,8 @@ namespace ef
         void selectBuilding(Pos start, Pos end);
         void stopUnit();
         void setTarget();
-        void placeBuilding(string building);
-        void produce(int producerId, string unitToProd, bool isTech);
+        void placeBuilding(std::string building);
+        void produce(int producerId, std::string unitToProd, bool isTech);
         bool DestroyUnit();
         bool DestroyBuilding();
         bool destroyUnit(int unitId, bool isOther);
@@ -84,9 +84,9 @@ namespace ef
         PlayerInfo playerInfo;
 
     private:
-        vector<shared_ptr<Unit>> selectedUnit;
-        vector<shared_ptr<Building>> selectedBuilding;
-        shared_ptr<UdpConnect> connectUdp;
+        std::vector<std::shared_ptr<Unit>> selectedUnit;
+        std::vector<std::shared_ptr<Building>> selectedBuilding;
+        std::shared_ptr<UdpConnect> connectUdp;
     }
 }; // !ef
 
