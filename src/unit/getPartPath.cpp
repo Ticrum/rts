e05aa997b871230818adf8d7d7798aa66f289f55
romain.piccoche <romain.picoche@laika.efrits.fr>
2024-03-25 11:34:36 +0100

    health if path = 0

--- src/unit/getPartPath.cpp
+++ src/unit/getPartPath.cpp
@@ -3,10 +3,13 @@
 std::vector<ef::ConformPos> ef::Unit::getPartPath()
 {
     std::vector<ConformPos> tempPath;
-    if (actualIndex == 0 || actualIndex + 1 == path.size())
+    if (actualIndex == 0 || actualIndex + 1 == path.size() || 0 == path.size())
       tempPath.push_back(pos.get());
     else
       tempPath.push_back(path[actualIndex - 1].get());
-    tempPath.push_back(path[actualIndex].get());
+    if(0 == path.size())
+      tempPath.push_back(pos.get());
+    else
+      tempPath.push_back(path[actualIndex].get());
     return tempPath;
 }
